5d4a605c5f715ce0bed50beace684a8e2b6458b0
Abdessalem-Biri <abdessalembiri@gmail.com>
2019-07-10 13:19:48 +0100

    hello karim

--- src/main/java/com/phoenix/exercises/ex1/FileUtils.java
+++ src/main/java/com/phoenix/exercises/ex1/FileUtils.java
@@ -1,5 +1,8 @@
 package com.phoenix.exercises.ex1;
 
-public class FileUtils {
+import com.phoenix.solution.ex1.FileUtils;
 
+public class FileUtils {
+	public static void main(String[] args) {
+		FileUtils  classUtils = new FileUtils() ; 
 }
